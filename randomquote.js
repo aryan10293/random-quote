f9be38cd15d7ff13107bd6857da84b0121a34041
Aondrej Ryan <aondrejryan11@gmail.com>
2020-11-05 23:25:15 -0800

    i did nothing

---
+++ randomquote.js
@@ -0,0 +1,16 @@
+let Quotes = [
+    {
+        quote: ' "Dont be pushed around by the fears in your mind. Be led by the dreams in your heart." ',
+        writer: 'Roy T. Bennett'
+    },
+    {
+        quote:' "It’s only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform." ',
+        writer: 'Roy T. Bennett'
+    }
+    {
+        quote: '"FUCK THEM KIDS"',
+        writer:'Michael Jordan'
+    }
+ ]
+
+let lebron = 'great basketball player ever'
